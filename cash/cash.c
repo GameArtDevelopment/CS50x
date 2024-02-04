800679d50d79a377b35d4aec53a1471abb4ab5fd
Joseph Prez <111929236+GameArtDevelopment@users.noreply.github.com>
2024-02-03 23:33:21 -0500

    Testing cash

--- cash/cash.c
+++ cash/cash.c
@@ -1,1 +1,11 @@
 #include <stdio.h>
+
+// customer inputs $1.00 (100cents) for .50cent candy
+// pay back (change) with min amount of coins
+// Prompt user for int > 0 (change owed: )
+// program re-prompts if int !> 0 and is not an int
+
+int main(void)
+{
+    printf("Change Owed:\n");
+}
