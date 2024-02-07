a78824e84ec352569854cf5b6a438c0d4179a8c1
Joseph Prez <111929236+GameArtDevelopment@users.noreply.github.com>
2024-02-06 23:00:44 -0500

    Added mario-more

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,46 +1,84 @@
 #include <stdio.h>
 
-void print_pyramid(int height);
+void print_row(int space, int size);
 
 int main(void)
 {
     int height;
-    do
+    printf("Height: ");
+    scanf("%d", &height);
+
+    while (height < 1 || height > 8)
     {
+        printf("Height must be between 1 and 8\n");
         printf("Height: ");
         scanf("%d", &height);
-    } while (height < 1 || height > 8);
+    }
 
-    print_pyramid(height);
+    for (int i = 0; i < height; i++)
+    {
+        print_row(height - i - 1, i + 1);
+    }
 
     return 0;
 }
 
-void print_pyramid(int height)
+void print_row(int space, int size)
 {
+    for (int i = 0; i < space; i++)
+    {
+        printf(" ");
+    }
+    for (int j = 0; j < size; j++)
+    {
+        printf("#");
+    }
+    printf("  ");
+    for (int j = 0; j < size; j++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
+#include <stdio.h>
+
+void print_row(int space, int size);
+
+int main(void)
+{
+    int height;
+    printf("Height: ");
+    scanf("%d", &height);
+
+    while (height < 1 || height > 8)
+    {
+        printf("Height must be between 1 and 8\n");
+        printf("Height: ");
+        scanf("%d", &height);
+    }
+
     for (int i = 0; i < height; i++)
     {
-        // Print spaces
-        for (int j = 0; j < height - i - 1; j++)
-        {
-            printf(" ");
-        }
-
-        // Print left side #
-        for (int k = 0; k < i + 1; k++)
-        {
-            printf("#");
-        }
-
-        // Print gap
-        printf("  ");
-
-        // Print right side #
-        for (int k = 0; k < i + 1; k++)
-        {
-            printf("#");
-        }
-
-        printf("\n");
+        print_row(height - i - 1, i + 1);
     }
+
+    return 0;
+}
+
+void print_row(int space, int size)
+{
+    for (int i = 0; i < space; i++)
+    {
+        printf(" ");
+    }
+    for (int j = 0; j < size; j++)
+    {
+        printf("#");
+    }
+    printf("  ");
+    for (int j = 0; j < size; j++)
+    {
+        printf("#");
+    }
+    printf("\n");
 }
